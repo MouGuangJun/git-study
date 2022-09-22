93552ef2eb118995b0d60e9187a7ce03b513bb40
MouGuangJun <1900794909@qq.com>
2022-09-22 15:58:21 +0800

    日记备份

---
+++ database/sql/mysql/Sql导出暂存文件.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: select * from user
+LIMIT 0, 1000
+
+-- Date: 2022-09-22 15:47
+*/
+INSERT INTO `` (`id`,`name`,`sex`,`age`) VALUES (1,'张三','男',18);
+INSERT INTO `` (`id`,`name`,`sex`,`age`) VALUES (2,'李四','1',19);
+INSERT INTO `` (`id`,`name`,`sex`,`age`) VALUES (3,'拉拉','2',17);
+INSERT INTO `` (`id`,`name`,`sex`,`age`) VALUES (4,'王五','MALE',18);
+INSERT INTO `` (`id`,`name`,`sex`,`age`) VALUES (5,'赵六','MALE',22);
+INSERT INTO `` (`id`,`name`,`sex`,`age`) VALUES (6,'李白','MALE',20);
+INSERT INTO `` (`id`,`name`,`sex`,`age`) VALUES (7,'赵信','1',21);
