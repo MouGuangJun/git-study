a4910da5c0d95c57a19284203f58e353974fb017
MouGuangJun <1900794909@qq.com>
2022-11-20 22:56:23 +0800

    日常SQL脚本修改

--- database/sql/mysql/script/20221118-01.sql
+++ database/sql/mysql/script/20221118-01.sql
@@ -1,2 +1,6 @@
 SELECT * FROM sp_manager;
 
+INSERT INTO `sp_manager` (`mg_id`, `mg_name`, `mg_pwd`, `mg_time`, `role_id`, `mg_mobile`, `mg_email`, `mg_state`) VALUES('512','admin1','$2y$10$0sKmRkkmpvhOn61GFWLQGOK4A3vNXF4mv8lmAryVUE3/OaiVOod/6','1668775694','-1','13587932543','1977985@qq.com',NULL);
+
+
+SELECT * FROM sp_role;
